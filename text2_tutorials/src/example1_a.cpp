a66393fa6524c192325da82c9cf37a2e7785a462
Max Wang <38257756+maxen0604@users.noreply.github.com>
2020-01-23 14:02:26 +0800

    替换 带注释版本

--- text2_tutorials/src/example1_a.cpp
+++ text2_tutorials/src/example1_a.cpp
@@ -1,23 +1,29 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include <sstream>
+#include "ros/ros.h"           //包含了ros节点所必要的文件
+#include "std_msgs/String.h"   //包含了所使用的消息类型
+#include <sstream>             //包含要使用的文件类型
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "example1_a");
-  ros::NodeHandle n;
+  ros::init(argc, argv, "example1_a"); //启动该节点并设置其名称,名称必须唯一
+  ros::NodeHandle n;                   //设置节点进程的句柄 
+
   ros::Publisher pub = n.advertise<std_msgs::String>("message", 1000);
-  ros::Rate loop_rate(10);
-  while (ros::ok())
+/*将节点实例化，设置成发布者,并将所发布的主题和类型（字符串string）的名称告知节点管理器。
+第一个参数是消息的名称:message,第二个是缓冲区道大小。
+如果主题发布数据速度较快,那么将缓冲区设置为1000个消息。*/
+  ros::Rate loop_rate(10);//设置发送数据道频率为10Hz 
+  
+  while (ros::ok())//当收到Ctrl+C的按键消息或ROS停止当前节点运行时,ros::ok()库会执行停止节点运行道命令  
   {
-    std_msgs::String msg;
-    std::stringstream ss;
+//////创建一个消息变量,变量类型必须符合发送数据的要求  
+    std_msgs::String msg;  //定义msg
+    std::stringstream ss;  //
     ss << " I am the example1_a node ";
     msg.data = ss.str();
     //ROS_INFO("%s", msg.data.c_str());
-    pub.publish(msg);
-    ros::spinOnce();
-    loop_rate.sleep();
+    pub.publish(msg);//消息被发布 
+    ros::spinOnce();//如果一个订阅者出现,ROS就会更新和读取所有主题  
+    loop_rate.sleep();//按照10Hz的频率将程序挂起 
   }
   return 0;
 }
--- text2_tutorials/src/example1_b.cpp
+++ text2_tutorials/src/example1_b.cpp
@@ -1,16 +1,21 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-void messageCallback(const std_msgs::String::ConstPtr& msg)
+//设置回调函数messageCallback,每次该节点收到一条消息时都会调用此函数。该函数可以处理数据。
+void messageCallback(const std_msgs::String::ConstPtr& msg) 
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
 int main(int argc, char **argv)
 {
+  //初始化节点并设置名称(名称唯一)
   ros::init(argc, argv, "example1_b");
+//进程的处理程序，允许我们与环境交互
   ros::NodeHandle n;
+//将节点实例化为订阅者，并将所发布的主题类型的名称告知节点管理器。名为message。处理消息的函数为messageCallback
   ros::Subscriber sub = n.subscribe("message", 1000, messageCallback);
+//spin()函数负责处理节点开始读取主题和在消息到达时，回调函数messageCallback被调用的主循环。Ctrl+C组合建，节点退出消息循环，于是结束循环
   ros::spin();
   return 0;
 }
